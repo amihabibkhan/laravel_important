19cbfe9db5214c0209115b2ac17570dfdfa70865
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2019-03-23 10:35:28 +0600

    Laravel custom helper function create

---
+++ laravel_custom_helpers.php
@@ -0,0 +1,19 @@
+// have to make a file in app folder. Its best to make in app root
+
+// like app/helpers.php
+
+// now have to go to composer.json file and add this file as helper in autoload
+
+
+"autoload": {
+        "files": [
+            "app/helpers.php"
+        ],
+        "psr-4": {
+            "App\\": "app/"
+        },
+        "classmap": [
+            "database/seeds",
+            "database/factories"
+        ]
+    },
