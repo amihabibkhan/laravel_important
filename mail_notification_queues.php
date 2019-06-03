38db2318f44e2686503caf48da32b4051efb8a60
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2019-06-03 09:39:27 +0600

    Create mail_notification_queues.php

---
+++ mail_notification_queues.php
@@ -0,0 +1,20 @@
+// php artisan make:notification NotificationName
+
+Notification::send($admins, new PlaceOrderNotification($order));
+
+// $admins defines receiver
+// PlaceOrderNotification define Notification which created before
+// $order define datas which will be available in Notification
+
+
+// for queues
+php artisan queue:table
+php artisan migrate
+
+// change the driver from env file from sync to database
+QUEUE_DRIVER=database instead of QUEUE_CONNECTION=database
+
+// in Notification file have to implement ShouldQueue after extends of class like this
+
+class PlaceOrderNotification extends Notification implements ShouldQueue
+
