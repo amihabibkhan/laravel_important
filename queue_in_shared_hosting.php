1177557a1ded325b77a210fd5f7d9ac731032ae1
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2019-06-17 09:27:59 +0600

    Create queue_in_shared_hosting.php

---
+++ queue_in_shared_hosting.php
@@ -0,0 +1,49 @@
+// start cron with this
+  
+  /usr/bin/php /path/to/artisan schedule:run >/dev/null 2>&1
+
+// php.ini
+  register_argc_argv=On
+
+
+
+// in  Kernel.php 
+
+<?php
+namespace App\Console;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+    ];
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $path = base_path();
+        $schedule->call(function() use($path) {
+            if (file_exists($path . '/queue.pid')) {
+                $pid = file_get_contents($path . '/queue.pid');
+                $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+                $run = $result == '' ? true : false;
+            } else {
+                $run = true;
+            }
+            if($run) {
+                $command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:listen --tries=3 > /dev/null & echo $!';
+                $number = exec($command);
+                file_put_contents($path . '/queue.pid', $number);
+            }
+        })->name('monitor_queue_listener')->everyFiveMinutes();
+    }
+}
