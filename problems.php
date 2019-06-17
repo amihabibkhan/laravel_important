b6a80ba5d7b7cff2325bcca2f23fc67105932a5b
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2019-06-17 09:07:01 +0600

    Create problems.php

---
+++ problems.php
@@ -0,0 +1,7 @@
+// image intervention
+
+*) For use Intervension Must have to enabled fileinfo extension
+  -> In live server (fileinfo extension must have to enabled in your server)
+  
+*) Image source not readable
+  -> If you use public_path and storage please command the "storage:link" in live server
