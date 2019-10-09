109eeb2cc769bf1d8b51484f94810f14e7ab1c68
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2019-10-09 21:47:23 +0600

    Create web_mail_configuration_for_live_server.php

---
+++ web_mail_configuration_for_live_server.php
@@ -0,0 +1,11 @@
+MAIL_DRIVER=mail
+MAIL_HOST=premium44.web-hosting.com
+MAIL_PORT=465
+MAIL_USERNAME=support@domain.com
+MAIL_PASSWORD=password
+MAIL_ENCRYPTION=null
+
+// its for namecheap hosting
+MAIL_HOST=premium44.web-hosting.com
+
+mail_host have to collect from cpanel server information option and have to add .web-hosting.com
