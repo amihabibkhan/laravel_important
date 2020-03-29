0eca988f3286ba365b515e8014719672215473ba
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2020-03-30 00:49:54 +0600

    Create ck_editor_image_width.js

---
+++ ck_editor_image_width.js
@@ -0,0 +1,8 @@
+<script>
+        $(function () {
+            $(".post_description img").css({
+                'width' : '100%',
+                'height' : 'auto'
+            });
+        });
+</script>
