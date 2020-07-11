98be2852b7b95334903fb1b9b959502c5c2607ca
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2020-07-11 07:46:44 +0600

    Update serialNumberInLaravelPaginate.php

--- serialNumberInLaravelPaginate.php
+++ serialNumberInLaravelPaginate.php
@@ -1,1 +1,3 @@
-{{ $loop->index + 1 + ((request()->page != 1) ? request()->page * 10 : 0) }}
+// where paginate(30)
+
+<td>{{ $loop->index + 1 + ((request()->page == 1 || !request()->page) ? 0 : ((request()->page - 1) * 30)) }}</td>
