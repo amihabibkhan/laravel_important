2c7c516a79479eab70f81280fd00a100de7acab3
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2020-11-03 20:11:46 +0600

    Update bangla_slug_maker.php

--- bangla_slug_maker.php
+++ bangla_slug_maker.php
@@ -4,7 +4,8 @@ function slug_maker($text){
     // creating slug
     $remove_question = str_replace('?', ' ', $text);
     $remove_slash = str_replace('/', ' ', $remove_question);
-    $slug = preg_replace('/\s+/u', '-', trim($remove_slash));
+    $remove_dot = str_replace('.', ' ', $remove_question);
+    $slug = preg_replace('/\s+/u', '-', trim($remove_dot));
     return $slug;
 }
 
