e8feb4bae6749f57fdbb2639a14176261679fa86
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2021-01-02 08:25:28 +0600

    Update web_mail_configuration_for_live_server.php

--- web_mail_configuration_for_live_server.php
+++ web_mail_configuration_for_live_server.php
@@ -5,6 +5,24 @@ MAIL_USERNAME=support@domain.com
 MAIL_PASSWORD=password
 MAIL_ENCRYPTION=null
 
+
+
+// its also working
+MAIL_MAILER=smtp
+MAIL_HOST=premium162.web-hosting.com
+MAIL_PORT=465
+MAIL_USERNAME=info@domain.com
+MAIL_PASSWORD=password
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=info@sunnahfij.com
+MAIL_FROM_NAME="${APP_NAME}"
+
+
+
+
+
+
+
 // its for namecheap hosting
 MAIL_HOST=premium44.web-hosting.com
 
